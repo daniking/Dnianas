67461dab9d023dada64fe7fe3944bccc56384cf6
Akar Mathers <akarmathers13@gmail.com>
2014-12-14 23:26:37 +0300

    Added a page to show user profile

--- app/views/profile/user.blade.php
+++ app/views/profile/user.blade.php
@@ -3,7 +3,18 @@
     <li>First name: <strong>{{ $user->first_name }}</strong>   </li>
     <li>Last name:  <strong>{{ $user->last_name }}</strong> </li>
     <li>Birthday: <strong>{{ $user->birthday }}</strong> </li>
-    <li>Address: <strong>{{ $user->about->address }}</strong> </li>
-    <li>Job Title: <strong>{{ $user->about->job_title }}</strong> </li>
-    <li>{{ $user->about->about }}</li>
+    @if($user->about)
+        <li>Address: <strong>{{ $user->about->address}}</strong> </li>
+        <li>Job Title: <strong>{{ $user->about->job_title }}</strong> </li>
+        <li>About: <strong>{{ $user->about->about }}</strong></li>
+    @endif
 </ul>
+
+<br />
+<br />
+
+<h2>User posts</h2>
+<hr>
+@foreach($user->posts as $p) 
+    <div>{{ $p->post_content }}</div>
+@endforeach
