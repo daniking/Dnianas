85bdeadf415659086ea417cce95405fd3e693257
AkarM13 <akarmathers13@gmail.com>
2014-12-16 19:18:24 +0300

    Add moment.js and livestamp.js to the footer

--- app/views/home/_partials/footer.php
+++ app/views/home/_partials/footer.php
@@ -1,5 +1,7 @@
 <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
 <script src="js/init.js"></script>
+<script src="js/moment.min.js"></script>
+<script src="js/livestamp.min.js"></script>
 <script src='js/jquery.autosize.js'></script>
 <script src='js/dnianas.js'></script>
 <script>
