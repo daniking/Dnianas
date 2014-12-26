0f5c81aaa64221eb2783ba6157789dd9577a32a5
AkarM13 <akarmathers13@gmail.com>
2014-12-26 14:53:49 +0300

    Add docblocks

--- app/Dnianas/Forms/CommentForm.php
+++ app/Dnianas/Forms/CommentForm.php
@@ -1,5 +1,4 @@
-<?php
-
+<?php 
 namespace Dnianas\Forms;
 
 
@@ -7,6 +6,10 @@ use Laracasts\Validation\FormValidator;
 
 class CommentForm extends FormValidator
 {
+    /**
+     * Validation rules for comment form
+     * @var array
+     */
     protected $rules = [
         'post_id' => 'numeric|min:1|integer|exists:posts,id',
         'text'    => 'required'
