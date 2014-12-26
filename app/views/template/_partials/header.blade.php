dc0ac00183eb3b72e53481beadb2f09ae87183c4
AkarM13 <akarmathers13@gmail.com>
2014-12-26 22:00:38 +0300

    Add FontAwesome stylesheet

--- app/views/template/_partials/header.blade.php
+++ app/views/template/_partials/header.blade.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 <link href="{{ asset('style/index.css') }}" rel="stylesheet">
 <link href="{{ asset('style/style.css') }}" rel="stylesheet">
 <link href="{{ asset('style/jqstdniapara.css') }}" rel="stylesheet"> 
