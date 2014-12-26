9e4ce40130332f53c90117a9af1a769d32010e6a
AkarM13 <akarmathers13@gmail.com>
2014-12-26 14:54:43 +0300

    Add post liking functionality

--- app/Dnianas/Post/PostRepository.php
+++ app/Dnianas/Post/PostRepository.php
@@ -2,6 +2,7 @@
 namespace Dnianas\Post;
 
 use \Post;
+use User;
 
 class PostRepository
 {
@@ -33,4 +34,26 @@ class PostRepository
     {
         return Post::with('user')->where('id', '>', $id)->get();
     }
+
+    public function like($post_id, User $user)
+    {
+        return Post::find($post_id)->likes()->attach($user->id);
+    }
+
+    public function unlike($post_id, User $user)
+    {
+        return Post::find($post_id)->likes()->detach($user->id);
+    }
+
+    public function isLikedBy(User $user, $post_id) 
+    {
+        $likes = Post::find($post_id)->likes()->where('user_id', $user->id)->first();
+
+        // If the user likes that post
+        if ($likes) return true;
+
+        return false;
+    }
+
+
 }
