c3ed5dd93c8cf4a0ad089e078ab0d30db2b6232f
AkarM13 <akarmathers13@gmail.com>
2014-12-30 01:31:21 +0300

    Minor changes

--- app/views/home/_partials/footer.php
+++ app/views/home/_partials/footer.php
@@ -1,9 +1,9 @@
 <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-<script src="js/init.js"></script>
-<script src="js/moment.min.js"></script>
-<script src="js/livestamp.min.js"></script>
-<script src='js/jquery.autosize.js'></script>
-<script src='js/dnianas.js'></script>
+<script src="/js/init.js"></script>
+<script src="/js/moment.min.js"></script>
+<script src="/js/livestamp.min.js"></script>
+<script src="/js/jquery.autosize.js"></script>
+<script src='/js/dnianas.js'></script>
 <script>
     $(document).ready(function() {
         getNewPosts();
