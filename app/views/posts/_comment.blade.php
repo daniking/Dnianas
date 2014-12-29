51bab33702f8cc741f335f6c08a94a1d5a154239
AkarM13 <akarmathers13@gmail.com>
2014-12-30 01:33:05 +0300

    Minor changes

--- app/views/posts/_comment.blade.php
+++ app/views/posts/_comment.blade.php
@@ -3,8 +3,8 @@
         @if($post->comments)
             @foreach($post->comments as $comment)
                 <div class="comment">
-                    <img src="photo/pro.jpg" class="profile-picture">
-                    <a href="user/{{ $comment->user->username }}" class="comment-name">{{ $comment->fullName() }}</a>
+                    <img src="/photo/profile_picture.jpeg" class="profile-picture">
+                    {{ link_to_route('profile.show', $comment->user->fullName(), $comment->user->username, ['class' => 'comment-name']) }}
                     <div class="comment-text">{{ $comment->text }}</div>
                     <div class="posted-date" data-livestamp="{{ formatDate($comment->posted_date) }}"></div>
                 </div>
@@ -12,7 +12,7 @@
         @endif
     </div>
     <div class="comment-insert">
-        <img src="photo/pro.jpg" alt="Profile Picture" class="profile-picture">
+        <img src="/photo/profile_picture.jpeg" alt="Profile Picture" class="profile-picture">
         <textarea  dir="ltr" class="comment-input" placeholder="Write something..."></textarea>
     </div>
 </div>
