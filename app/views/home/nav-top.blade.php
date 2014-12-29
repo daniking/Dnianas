a81d6bb443e4706d47c0a6ebaaec484e665e314c
AkarM13 <akarmathers13@gmail.com>
2014-12-30 01:32:00 +0300

    Minor changes

--- app/views/home/nav-top.blade.php
+++ app/views/home/nav-top.blade.php
@@ -1,6 +1,6 @@
 <div class="header _top11 _fixs">
     <div class="main_con_hea">
-        <div class="logo">
+        <div class="logo" href="/">
             <p>dnianas</p>
         </div>
         <div class="search">
@@ -9,12 +9,12 @@
 
         </div>
         <div id="rig-ico-texts">
-            <a href="" class="text-top" id="hov_pic_shows"><img src="photo/pro.jpg" id="picprosmall" />{{ Auth::user()->first_name }}</a>
+            <a href="" class="text-top" id="hov_pic_shows"><img src="/photo/profile_picture.jpeg" id="picprosmall" />{{ Auth::user()->first_name }}</a>
             <a href="" class="text-top">Notifications</a>
             <a href="" class="text-top">Messages</a>
-            <a href=""><img src="img/icons/sett.png" class="icos"></a>
-            <a href=""><img src="img/icons/help.png" class="icos"></a>
-            <span id="logout"><img src="img/icons/off.png" class="icos"></span>
+            <a href=""><img src="/img/icons/sett.png" class="icos"></a>
+            <a href=""><img src="/img/icons/help.png" class="icos"></a>
+            <span id="logout"><img src="/img/icons/off.png" class="icos"></span>
         </div>
 
     </div>
