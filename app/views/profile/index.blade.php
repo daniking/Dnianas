91f9163fc9b2f5c2c55cbfbb27d9db28ca0f9ba2
AkarM13 <akarmathers13@gmail.com>
2014-12-31 16:28:08 +0300

    Change data attribute names

--- app/views/profile/index.blade.php
+++ app/views/profile/index.blade.php
@@ -10,11 +10,11 @@
                     <span>Messages</span>
                 </button>
                 @if($user->isFollowedBy(Auth::user(), $user->id))
-                    <button class="bot_sasda22131 following" id="followBtn" data-action="unfollow" data-profileid="{{ $user->id }}">
+                    <button class="bot_sasda22131 following" id="followBtn" data-action="unfollow" data-profile-id="{{ $user->id }}">
                         <span>Following</span>
                     </button>
                 @else 
-                <button class="bot_sasda22131" id="followBtn" data-action="follow" data-profile_id="{{ $user->id }}">
+                <button class="bot_sasda22131" id="followBtn" data-action="follow" data-profile-id="{{ $user->id }}">
                     <span>Follow</span>
                 </button>                
                 @endif
