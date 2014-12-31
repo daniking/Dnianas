57e906d060548e1a1db8de199193db45b4aaa1d8
AkarM13 <akarmathers13@gmail.com>
2014-12-31 14:35:43 +0300

    Reformating and some cleanup

--- public/js/init.js
+++ public/js/init.js
@@ -1,14 +1,15 @@
-$(document).ready(function() {
-    token = $('input[name=_token]').val();
-    // When someone posts
-    $('body').on('submit', '#postForm', function(event) {     
-        $.ajax({
-            url: '/posts/create',
-            type: 'POST',
-            dataType: 'JSON',
-            data: $(this).serialize()
-        })
-        .done(function(data) {
+var token = $('input[name=_token]').val();
+var $body = $('body');
+
+// When someone posts
+$body.on('submit', '#postForm', function (event) {
+    $.ajax({
+        url: '/posts/create',
+        type: 'POST',
+        dataType: 'JSON',
+        data: $(this).serialize()
+    })
+        .done(function (data) {
             if (data.success == 'false') {
                 $('.error').empty().append(data.message).slideDown(200).delay(3000).slideUp(250);
             } else {
@@ -19,26 +20,26 @@ $(document).ready(function() {
             }
         })
 
-        event.preventDefault();
-    });
+    event.preventDefault();
+});
 
-    $('body').on('click', '#likePost', (function(event) {
-        // The post id
-        var $post_id        = $(this).parents().get(2).dataset.id;
-        $postLike           = $(this);
-        var $postLikeEl     = $postLike.children('#likeCount');
-        var $postLikeCount  = $postLikeEl.text();
-        $.ajax({
-            url: '/posts/like',      
-            type: 'POST',
-            dataType: 'JSON',
-            data: {
-                post_id: $post_id,
-                like_count: $postLikeCount,
-                _token: token
-            },
-        })
-        .done(function(data) {
+$body.on('click', '#likePost', (function (event) {
+    // The post id
+    var $post_id = $(this).parents().get(2).dataset.id;
+    var $postLike = $(this);
+    var $postLikeEl = $postLike.children('#likeCount');
+    var $postLikeCount = $postLikeEl.text();
+    $.ajax({
+        url: '/posts/like',
+        type: 'POST',
+        dataType: 'JSON',
+        data: {
+            post_id: $post_id,
+            like_count: $postLikeCount,
+            _token: token
+        }
+    })
+        .done(function (data) {
             if (data.like) {
                 $postLike.children('.likeIcon').addClass('liked');
                 $postLikeEl.addClass('liked');
@@ -48,76 +49,75 @@ $(document).ready(function() {
             }
             $postLikeEl.text(data.like_count);
         })
-        
-    }));
-});
 
-    // The short polling process
-    function getNewPosts() {
-        if ($('.poster_memb .tab_post').length) {
+}));
 
-            // Map the div to array
-            var ids = $('.poster_memb .tab_post').map(function() {
+// The short polling process
+function getNewPosts() {
+    var $postEl = $('.poster_memb .tab_post');
+    if ($postEl.length) {
 
-                // Get the ids and store them in this array
-                return +$(this).data('id') || 0;
-            });
+        // Map the div to array
+        var ids = $postEl.map(function () {
 
-            // Find the largest id from the ids array and set it to a variable
-            last_id = Math.max.apply(Math, ids);
-        } else {
-            last_id = 0;
-        }
-        
-        $.ajax({
-            url: '/posts/latest/' + last_id,
-            type: 'GET',
-            dataType: 'JSON'
-        })
-        .done(function(data) {
+            // Get the ids and store them in this array
+            return +$(this).data('id') || 0;
+        });
+
+        // Find the largest id from the ids array and set it to a variable
+        var last_id = Math.max.apply(Math, ids);
+    } else {
+        last_id = 0;
+    }
+
+    $.ajax({
+        url: '/posts/latest/' + last_id,
+        type: 'GET',
+        dataType: 'JSON'
+    })
+        .done(function (data) {
             if (data) {
                 if (data.is_new = 'true' && data.html) {
                     $('.no-posts').hide();
-                    $(data.html).insertAfter('#postForm').hide().slideDown(500);    
+                    $(data.html).insertAfter('#postForm').hide().slideDown(500);
                 }
-                
+
             }
 
             // Run the function every 10 seconds
             setTimeout(getNewPosts, 10000);
         })
 
-    };
+};
 
-    // When a user presses 'enter' on the comment field
-    $('body').on('keypress', '.comment-input', function(event) {        
-        if (event.which == 13 && !event.shiftKey) {
-            // Grab the post id and other inputs
-            var $post_id        = $(this).parents().get(3).dataset.id;
-            var $comment_input  = $(this);
-            $.ajax({
-                url: '/comment/create',
-                type: 'POST',
-                dataType: 'JSON',
-                data: {
-                    text    : $comment_input.val(),
-                    _token  : token,
-                    post_id : $post_id
-                }
-            })
-            .done(function(data) {
+// When a user presses 'enter' on the comment field
+$body.on('keypress', '.comment-input', function (event) {
+    if (event.which == 13 && !event.shiftKey) {
+        // Grab the post id and other inputs
+        var $post_id = $(this).parents().get(3).dataset.id;
+        var $comment_input = $(this);
+        $.ajax({
+            url: '/comment/create',
+            type: 'POST',
+            dataType: 'JSON',
+            data: {
+                text: $comment_input.val(),
+                _token: token,
+                post_id: $post_id
+            }
+        })
+            .done(function (data) {
                 $(data.html).insertBefore($comment_input.parent());
+            });
 
-            })
-
-            // Reset the input field
-            $(this).val('').blur();
-        }
+        // Reset the input field
+        $(this).val('').blur();
+    }
 });
 
-$('#followBtn').click(function(event) {
-    var $followBtn      = $(this);
-    var profileId       = $followBtn.data('profileid');
+$('#followBtn').click(function (event) {
+    var $followBtn = $(this);
+    var profileId = $followBtn.data('profileid');
 
     $.ajax({
         url: '/follow',
@@ -126,13 +126,13 @@ $('#followBtn').click(function(event) {
         data: {
             profile_id: profileId,
             _token: token
-        },
-    })
-    .done(function(data) {
-        if (data.follow) {
-            $followBtn.text('Unfollow');
-        } else {
-            $followBtn.text('Follow');
         }
     })
+        .done(function (data) {
+            if (data.follow) {
+                $followBtn.text('Unfollow');
+            } else {
+                $followBtn.text('Follow');
+            }
+        })
 });
