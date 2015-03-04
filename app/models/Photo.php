bcde8c6957e3979a929cb5089a81b2ac06461829
AkarM13 <akarmathers13@gmail.com>
2015-03-04 18:28:25 +0300

    Remove unnecessary methods

--- app/models/Photo.php
+++ app/models/Photo.php
@@ -8,22 +8,6 @@ class Photo extends Eloquent
     {
         return $this->morphTo();
     }
-    /**
-     * Get the user profile picture path
-     * @return object The path to the user profile picture
-     */
-    public function profilePicture()
-    {
-        return ($this->profile_picture == true) ? ($this->path) : '';
-    }
 
-     /**
-     * Get the user cover photo path
-     * @return object The path to the user cover photo
-     */
-    public function coverPhoto()
-    {
-        return ($this->cover_photo == true) ? ($this->path) : '';
-    }
 
 }
