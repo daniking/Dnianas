0f84e05d928338fae7640feabe6ddfbd5ee47058
AkarM13 <akarmathers13@gmail.com>
2015-03-04 10:36:44 +0300

    Add JavaScript file to upload profile picture.

---
+++ public/js/imageUpload.js
@@ -0,0 +1,36 @@
+var token = $('input[name=_token]').val();
+
+$('#profilePicture').on('change', function(event) {
+    var profilePictureEl = $('#profilePicture');
+    var profilePicture = profilePictureEl.get(0).files[0];
+   // Check if the browser is supporting the File API.
+   if (window.File && window.FileReader && window.FileList && window.Blob) {
+        var formData = new FormData();
+        formData.append('profile_picture', profilePicture);
+        formData.append('_token',token);
+        $.ajax({
+            url: '/getting_started/step_two/profile_picture',
+            type: 'POST',
+            dataType: 'JSON',
+            processData: false,
+            contentType: false,
+            data: formData
+        })
+        .done(function(data) {
+            pictureBox = profilePictureEl.parents(2).next('#pictrigbox');
+            pictureBox.empty().prepend('<img id="profilePicturePreview" src="'+ data.image_path +'" />');
+        })
+        
+    } else {
+        // If it doesn't support File API.
+        alert('Your browser is outdated. Please upgrade your browser to access this functionality!');
+    }
+    event.preventDefault();
+});
+
+$('#coverPhoto').on('change', function(event) {
+    
+    console.log('Cover photo has been selected');
+    // Prevent the default action
+    event.preventDefault();
+});
