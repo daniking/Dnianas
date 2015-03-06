0d18f2b6a34fc4a54dc3145626ed880c6b906ac9
AkarM13 <akarmathers13@gmail.com>
2015-03-06 20:49:45 +0300

    Add a title

--- app/views/auth/step_one.blade.php
+++ app/views/auth/step_one.blade.php
@@ -9,29 +9,28 @@
     <h1 class="tiitleupd">Set Your information</h1>
     @include('template._partials.error')
     {{ Form::open(['url' => 'getting_started']) }}            
-        <label class="text_text_info">
+    <label class="text_text_info">
         <input type="text" name="address" class="text_box_info" placeholder="Address"></label>
         <hr class="ssd323">
         <label class="text_text_info">
             <input type="text" name="job_title" class="text_box_info" placeholder="Work at">
         </label>
+        <hr class="ssd323">
+        <label class="text_text_info">
+            <input type="text" name="website" class="text_box_info" placeholder="Website"></label>
             <hr class="ssd323">
-            <label class="text_text_info">
-                <input type="text" name="website" class="text_box_info" placeholder="Website"></label>
-                <hr class="ssd323">
-                <label class="httext">About You
-                    <hr class="srxcs">
-                    <textarea class="_140words" name="about"></textarea><br>
+            <label class="httext">About You
+                <hr class="srxcs">
+                <textarea class="_140words" name="about"></textarea><br>
 
-                    <span id="tips">Write about yourself in 140 characters 
+                <span id="tips">Write about yourself in 140 characters 
 
-                    </label>
+                </label>
 
-                    <hr class="underline">
-                    <input type="submit" value="Save & Continue" name="" class="sa_co">
-                    <a href class="skipss">Skip This Step</a>
-                    <br><br>
-                    {{ Form::close() }}
-                </div>
-            </div>
-
+                <hr class="underline">
+                <input type="submit" value="Save & Continue" name="" class="sa_co">
+                <a href class="skipss">Skip This Step</a>
+                <br><br>
+                {{ Form::close() }}
+        </div>
+</div>
