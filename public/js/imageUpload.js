d4014fddd1c52915b5dd7ca69b28d79e87728b76
AkarM13 <akarmathers13@gmail.com>
2015-03-07 22:10:49 +0300

    Add a method to handle the cover photo upload

--- public/js/imageUpload.js
+++ public/js/imageUpload.js
@@ -29,8 +29,32 @@ $('#profilePicture').on('change', function(event) {
 });
 
 $('#coverPhoto').on('change', function(event) {
-    
-    console.log('Cover photo has been selected');
-    // Prevent the default action
+    var coverPhotoEl = $('#coverPhoto');
+    var coverPhoto = coverPhotoEl.get(0).files[0];
+   // Check if the browser is supporting the File API.
+   if (window.File && window.FileReader && window.FileList && window.Blob) {
+        var formData = new FormData();
+        formData.append('cover_photo', coverPhoto);
+        formData.append('_token',token);
+        $.ajax({
+            url: '/getting_started/step_two/cover_photo',
+            type: 'POST',
+            dataType: 'JSON',
+            processData: false,
+            contentType: false,
+            data: formData
+        })
+        .done(function(data) {
+            if(data.success === 'false') {
+                
+            }
+            pictureBox = coverPhotoEl.parents(2).next('#pictrigbox');
+            pictureBox.empty().prepend('<img id="coverPhotoPreview" src="'+ data.image_path +'" />');
+        })
+        
+    } else {
+        // If it doesn't support File API.
+        alert('Your browser is outdated. Please upgrade your browser to access this functionality!');
+    }
     event.preventDefault();
 });
