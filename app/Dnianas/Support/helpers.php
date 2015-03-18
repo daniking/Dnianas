68cad9d675b5650ecb1722b16fd0a4678936d2dc
AkarM13 <akarmathers13@gmail.com>
2015-03-18 17:16:07 +0300

    Fix bug

--- app/Dnianas/Support/helpers.php
+++ app/Dnianas/Support/helpers.php
@@ -11,17 +11,18 @@ function photos_path()
 
 function profile_picture($user) 
 {
-    if($user && !is_null($user->profile_picture)) {
+    if($user && $user->profile_picture) {
         return url('/' . 'photos/' . $user->profile_picture);
+    } else {
+        return url('/photos/profile_picture.jpg');
     }
-    
-    return url('/photos/profile_picture.jpg');
-
 }
 function cover_photo($user)
 {
-    if(!is_null($user->cover_photo)) {
+    if($user && $user->cover_photo) {
         return url('/' . 'photos/cover-' . $user->cover_photo);
     }
+
+    return url('/' . 'photos/cover-675f995bcba572f1a5a6b47002a33cba2cef0d8c.jpg');
 
 }
