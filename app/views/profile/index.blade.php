056e868930da9830cbf9274f9f692a7d6f36abc5
AkarM13 <akarmathers13@gmail.com>
2015-03-18 17:17:19 +0300

    Add some checks before displaying info about the user

--- app/views/profile/index.blade.php
+++ app/views/profile/index.blade.php
@@ -51,14 +51,20 @@
         </div>
         <div class="leftabot" id="memb_sos">
             <h1>About</h1>
+            @if($user->about && $user->about->count()) 
             <h2>Live in <span>{{ $user->about->address }}</span></h2>
             <h2>Job Title<span>{{ $user->about->job_title }}</span></h2>
             <h2>Website <span>{{ $user->about->website }}</span></h2>
 
-            <p class="text-about">About You</p>
+            <p class="text-about">About</p>
             <p class="about-you" dir="ltr">
                 {{ $user->about->about }}
            </div>
+
+           @else
+            <p class="about-you" dir="ltr"></p>
+           </div>           
+           @endif
            <div  id="user_hoprofily">
                 @if( $user->id == Auth::user()->id )
                     <div class="post-add" style="margin-left: 35px">
