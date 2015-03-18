057d4f0a30319ce752953c975c28eac2845fe543
AkarM13 <akarmathers13@gmail.com>
2015-03-18 16:31:12 +0300

    You shouldn't display follow button for current user profile

--- app/views/profile/index.blade.php
+++ app/views/profile/index.blade.php
@@ -6,11 +6,10 @@
             <img src="{{ cover_photo($user) }}">
             <div class="ri_messs_fo232">
                 
+                @if(Auth::user()->id !== $user->id)
                 <button class="bot_sasda22131" id="openmsg">
                     <span>Messages</span>
                 </button>
-
-                @if(!Auth::user() === $user)
                     @if($user->isFollowedBy(Auth::user(), $user->id))
                         <button class="bot_sasda22131 following" id="followBtn" data-action="unfollow" data-profile-id="{{ $user->id }}">
                             <span>Following</span>
@@ -18,10 +17,8 @@
                     @else 
                     <button class="bot_sasda22131" id="followBtn" data-action="follow" data-profile-id="{{ $user->id }}">
                         <span>Follow</span>
-                    </button>                
+                    </button>
                     @endif
-                @else
-
                 @endif
             </div>
 
