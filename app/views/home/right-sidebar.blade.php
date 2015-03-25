3700d65082ffe88bc95ea8621f904e269c6a74bc
AkarM13 <akarmathers13@gmail.com>
2015-03-25 10:51:46 +0300

    Fix spelling and grammar

--- app/views/home/right-sidebar.blade.php
+++ app/views/home/right-sidebar.blade.php
@@ -1,12 +1,12 @@
 <div class="rig_lis_boo">
     <div id="u-like-book">
-        <h1>You Like Read Book</h1>
+        <h1>What would you like to read?</h1>
     </div>
     <div id="addsponsor">
         <p>Advertising</p>
     </div>
     <div id="pageshow_footer">
-        <p>Dnianas &copy; 2014 -
+        <p>Dnianas &copy; 2015-
             <a href="">Privacy</a> -
             <a href="">Terms</a> -
             <a href="">Help</a> -
