bab948fe7e02dede78bf58075480f974fd37127a
AkarM13 <akarmathers13@gmail.com>
2015-03-25 11:12:00 +0300

    Add top bar section to a view partial

---
+++ app/views/profile/_top-bar.blade.php
@@ -0,0 +1,11 @@
+
+<div class="ab_prof_mem">
+    <div id="user_lin_192">
+        <a href="" id="lf_lin_us" class="selc"><span>{{ $user->fullName()}}</span></a>
+        <a href="" id="lf_lin_us" ><span>Photos</span></a>
+        <a href="" id="lf_lin_us"><span>Books</span></a>
+        <a href="" id="lf_lin_us"><span>About</span></a>
+        <a href="" id="lf_lin_us"><span>Followers</span></a>
+        <a href="" id="lf_lin_us"><span>Following</span></a>
+    </div>
+</div>
