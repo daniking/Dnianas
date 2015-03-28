24e710b952109718a3a6ce2d29802e1c1fd0c2d4
AkarM13 <akarmathers13@gmail.com>
2015-03-28 18:14:46 +0300

    Change database name

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => '127.0.0.1',
-			'database'  => 'test',
+			'database'  => 'dnianas',
 			'username'  => 'akar',
 			'password'  => 'raparen',
 			'charset'   => 'utf8',
