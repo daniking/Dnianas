b02bcc8c8536381b812a9a65318552b5ebf48489
AkarM13 <akarmathers13@gmail.com>
2015-04-01 23:00:14 +0300

    Display the AJAX loader before posting

--- app/views/home/post-add.blade.php
+++ app/views/home/post-add.blade.php
@@ -6,6 +6,7 @@
             <a href class="ri_st11"><span>Update Status</span></a>
             <a href class="ri_st11"><span>Add Photos/Video</span></a>
             <a href class=""><span>Add Books</span></a>
+            <img src="/img/ajax_loader.gif" id="ajax-loader" style="display:none">
             <input type="submit" class="share-post" value="Share">
         </div>
 {{ Form::close() }}
