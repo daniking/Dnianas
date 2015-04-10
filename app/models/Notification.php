55613a77b496d9da5eabed02c29db3faa424a20d
AkarM13 <akarmathers13@gmail.com>
2015-04-10 15:13:28 +0300

    Add fillable properties

--- app/models/Notification.php
+++ app/models/Notification.php
@@ -2,11 +2,21 @@
 
 class Notification extends Eloquent
 {   
+    protected $fillable = [
+        'sender_id', 
+        'recipient_id', 
+        'object_id', 
+        'object_type', 
+        'notification_type', 
+        'link', 
+        'seen'
+    ];
+    public $timestamps = true;
 
     // The user that does the activity
-    public function user()
+    public function sender()
     {
-        return $this->belongsTo('User');
+        return $this->belongsTo('User', 'sender_id');
     }
 
     // The reciever of that notification
