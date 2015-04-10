df75f0277a9549b96beed723051bb5de2cebcff0
AkarM13 <akarmathers13@gmail.com>
2015-04-10 23:06:53 +0300

    Add notification view.

--- app/views/home/nav-top.blade.php
+++ app/views/home/nav-top.blade.php
@@ -9,7 +9,30 @@
             <a href="{{ route('profile.show', Auth::user()->username) }}" class="text-top" id="hov_pic_shows">
                 <img src="{{ profile_picture(Auth::user()) }}" id="picprosmall" /> {{ Auth::user()->first_name }}
             </a>
-            <a href="" class="text-top">Notifications</a>
+            <span class="text-top" id="opennotifii">Notifications</span>
+            <span class="not_nu1">250</span></span>
+            <div id="_whnb12">
+                <div class="boxnotifi _12nca">
+                    <span class="_212xls"></span>
+                    <div id="headernotifitop">
+                        <h2>Notifications</h2>
+                    </div>
+                    <div class="boxnotificationsusers">
+                        <div id="boxsendnotifi">
+                            <img src="photo/dani-pro.jpg" id="picnotifishow">
+                            <span id="namenotifishow">Nali W.Mahmood <span id="textnotifishow">Like This Photo</span></span>
+                            <img src="photo/aren.jpg" class="innotifi" >
+                        </div>
+                        <div id="boxsendnotifi">
+                            <img src="photo/dani-pro.jpg" id="picnotifishow">
+                            <span id="namenotifishow">Akar Muhamad<span id="textnotifishow">Like This Photo</span></span>
+                            <img src="photo/aren.jpg" class="innotifi" >
+                        </div>
+                    </div>
+                    <div id="footernotifitop"></div>
+                </div>
+            </div>
+            
             <a href="" class="text-top">Messages</a>
             <a href=""><img src="/img/icons/sett.png" class="icos"></a>
             <a href=""><img src="/img/icons/help.png" class="icos"></a>
