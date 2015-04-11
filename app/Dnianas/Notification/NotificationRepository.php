1af2113d0448cbb5396de3b8e32a03bb23f51ca0
AkarM13 <akarmathers13@gmail.com>
2015-04-11 23:44:04 +0300

    Add a method to get the latest notifications for the user.

--- app/Dnianas/Notification/NotificationRepository.php
+++ app/Dnianas/Notification/NotificationRepository.php
@@ -9,8 +9,14 @@ class NotificationRepository
      * Get notifications for provided user
      * @var $user The user you want to get the notifications for.
     */
-    public function latest(User $user)
+    public function latest(\User $user)
     {
-    
+        $notifications = Notification::with('sender')
+        ->where('recipient_id', $user->id)
+        ->where('sender_id', '!=', $user->id)
+        ->latest()
+        ->get();
+
+        return $notifications;
     }
 }
