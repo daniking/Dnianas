042fb7e5a7840f11608bc0338debb649c8d2e149
AkarM13 <akarmathers13@gmail.com>
2015-04-11 23:08:51 +0300

    Add NotificationRepository file

---
+++ app/Dnianas/Notification/NotificationRepository.php
@@ -0,0 +1,16 @@
+<?php 
+namespace Dnianas\Notification;
+
+use \Notification;
+
+class NotificationRepository 
+{   
+    /**
+     * Get notifications for provided user
+     * @var $user The user you want to get the notifications for.
+    */
+    public function latest(User $user)
+    {
+    
+    }
+}
