2fce4938a20a28824a0d7392ad9197b11394b886
AkarM13 <akarmathers13@gmail.com>
2015-04-11 21:00:19 +0300

    Add a notifications view directory

---
+++ app/views/notifications/show.blade.php
@@ -0,0 +1,25 @@
+<span class="text-top" id="opennotifii">
+    Notifications
+    <span class="not_nu1">2</span>
+</span>
+<div id="_whnb12">
+    <div class="boxnotifi _12nca">
+        <span class="_212xls"></span>
+        <div id="headernotifitop">
+            <h2>Notifications</h2>
+        </div>
+        <div class="boxnotificationsusers">
+            <div id="boxsendnotifi">
+                <img src="photo/dani-pro.jpg" id="picnotifishow">
+                <span id="namenotifishow">Nali W.Mahmood <span id="textnotifishow">Like This Photo</span></span>
+                <img src="photo/aren.jpg" class="innotifi" >
+            </div>
+            <div id="boxsendnotifi">
+                <img src="photo/dani-pro.jpg" id="picnotifishow">
+                <span id="namenotifishow">Akar Muhamad<span id="textnotifishow">Like This Photo</span></span>
+                <img src="photo/aren.jpg" class="innotifi" >
+            </div>
+        </div>
+        <div id="footernotifitop"></div>
+    </div>
+</div>
