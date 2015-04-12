2437a92854f7db0b6bc47f43fb9be60f70ef0299
AkarM13 <akarmathers13@gmail.com>
2015-04-12 22:58:14 +0300

    Add the send method.

--- app/Dnianas/Notification/NotificationRepository.php
+++ app/Dnianas/Notification/NotificationRepository.php
@@ -19,4 +19,25 @@ class NotificationRepository
 
         return $notifications;
     }
+
+    /**
+     * Send the notification to the user.
+     * @param  $sender_id           The sender of that notification
+     * @param  $recipient_id        The recipient of that notification
+     * @param  $object_id           The id of that object
+     * @param  $object_type         The object type (post, user, photo...)
+     * @param  notification_type    The notification type (like, follow, message)
+     * @return collection
+     */
+    public function send($sender_id, $recipient_id, $object_id, $object_type, $notification_type)
+    {
+        Notification::firstOrCreate([
+            'sender_id' => $user->id,
+            'recipient_id' => $profile_id,
+            'object_id' => $profile_id,
+            'object_type' => 'User',
+            'notification_type' => 'Follow',
+            'seen' => 0,
+        ]);
+    }
 }
