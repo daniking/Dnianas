974d69d113c84a3639cd3670128e66e8b0f0431f
AkarM13 <akarmathers13@gmail.com>
2015-04-15 21:17:25 +0300

    Add notification count for new notifications.

--- app/views/notifications/show.blade.php
+++ app/views/notifications/show.blade.php
@@ -1,7 +1,7 @@
 <span class="text-top" id="opennotifii">
     Notifications
-    @if($notifications && $notifications->count() )
-        <span class="not_nu1">{{ $notifications->count() }}</span>
+    @if($notifications && $notifications->count())
+        <span class="not_nu1">{{ $notificationCount->count() }}</span>
     @endif
 </span>
 <div id="_whnb12">
@@ -12,19 +12,25 @@
         </div>
         <div class="boxnotificationsusers">
             @foreach($notifications as $notification)
-                <a href="/"><div id="boxsendnotifi" data-id="{{ $notification->id }}">
-                    <img src="{{ profile_picture($notification->sender) }}" id="picnotifishow">
-                    <span id="namenotifishow">{{ $notification->sender->first_name }}
-                        @if($notification->notification_type == 'Like')
-                            <span id="textnotifishow">likes your status.</span>
-                        @elseif($notification->notification_type == 'Follow')
-                            <span id="textnotifishow">followed you.</span>
-                        @elseif($notification->notification_type == 'Comment')
-                            <span id="textnotifishow">commented on your post.</span>
-                        @endif
-                        <div class="notification-time" data-livestamp="{{ format_date($notification->created_at) }}"></div>
-                    </span>
-                </div></a>
+                <a href="/">
+                    @if($notification->seen == 0)
+                        <div id="boxsendnotifi" class="unread" data-id="{{ $notification->id }}">
+                    @else
+                        <div id="boxsendnotifi" data-id="{{ $notification->id }}">
+                    @endif
+                        <img src="{{ profile_picture($notification->sender) }}" id="picnotifishow">
+                        <span id="namenotifishow">{{ $notification->sender->first_name }}
+                            @if($notification->notification_type == 'Like')
+                                <span id="textnotifishow">likes your status.</span>
+                            @elseif($notification->notification_type == 'Follow')
+                                <span id="textnotifishow">followed you.</span>
+                            @elseif($notification->notification_type == 'Comment')
+                                <span id="textnotifishow">commented on your post.</span>
+                            @endif
+                            <div class="notification-time" data-livestamp="{{ format_date($notification->created_at) }}"></div>
+                        </span>
+                    </div>
+                </a>
             @endforeach
         </div>
         <div id="footernotifitop"></div>
