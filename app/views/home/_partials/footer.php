162bb75816719525f5de8a3fea91ed8cc2990457
AkarM13 <akarmathers13@gmail.com>
2015-04-20 23:24:58 +0300

    Move include scripts.

--- app/views/home/_partials/footer.php
+++ app/views/home/_partials/footer.php
@@ -1,8 +1,8 @@
 <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-<script src="/js/init.js"></script>
 <script src="/js/vendor/moment.min.js"></script>
 <script src="/js/vendor/livestamp.min.js"></script>
 <script src="/js/vendor/jquery.autosize.js"></script>
+<script src="/js/init.js"></script>
 <script src='/js/dnianas.js'></script>
 <script>
     $(document).ready(function() {
