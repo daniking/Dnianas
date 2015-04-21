23f0df3e80eb96a47b316e003dc2727ff3f727a3
AkarM13 <akarmathers13@gmail.com>
2015-04-21 09:20:46 +0300

    Add refactored version of init.js

---
+++ public/js/init2.js
@@ -0,0 +1,60 @@
+var Dnianas = Dnianas || {};
+
+var $loading    = $('#ajax-loader'),
+    token       = $('input[name=_token]').val(),
+    $body       = $('body');
+
+// The default AJAX config.
+$.ajaxSetup({
+    type: 'POST',
+    dataType: 'JSON',
+});
+
+// The Post object for posting related stuff.
+Dnianas.Post = {
+
+    init: function() {
+        this.bindEvents();
+    },
+
+    bindEvents: function() {
+        $body.on('submit', $('#postForm'), this.createPost);
+    },
+
+    createPost: function(event) {
+        var self = Dnianas.Post;
+        var data = {
+            post_content: $(this).find('textarea').val(),
+            _token: token
+        };
+        var request = $.ajax(
+        {
+            url: '/posts/create',
+            data: data,
+            beforeSend: function() {
+                $loading.show();
+            },
+            complete: function() {
+                $loading.hide();
+            }
+        });
+
+        request.done(function(data) {
+            self.renderCreatedPost(data);
+        });
+
+        event.preventDefault();
+    },
+
+    renderCreatedPost: function(data) {
+        if (data.success == 'false') {
+            $('.error').empty().append(data.message).slideDown(100).delay(3000).slideUp(250);
+        } else {
+            $('#postForm')[0].reset();
+            $(data.post_html).insertAfter('#postForm').hide().delay(100).slideDown(400);
+            $('.no-posts').hide();
+        }
+    }
+};
+
+Dnianas.Post.init();
