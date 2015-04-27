a7d627287ee274897da7ea15234cf5d68eee63b3
AkarM13 <akarmathers13@gmail.com>
2015-04-27 18:43:39 +0300

    Only check for new posts when the user is on the home page.

--- app/views/template/_partials/footer.blade.php
+++ app/views/template/_partials/footer.blade.php
@@ -4,5 +4,11 @@
 <script src='/js/vendor/jquery.autosize.js'></script>
 <script src='/js/dnianas.js'></script>
 <script src="/js/init2.js"></script>
+<script type="text/javascript">
+    var page = window.location.pathname;
+    if(page === '/') {
+        Dnianas.Post.getNewPosts();
+    }
+</script>
 </body>
 </html>
