5c9105c0818306de8468a35eb3dfe3810d678157
AkarM13 <akarmathers13@gmail.com>
2015-04-27 18:42:15 +0300

    Remove home partial footer.

--- app/views/home/_partials/footer.php
+++
@@ -1,9 +0,0 @@
-<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-<script src="/js/vendor/moment.min.js"></script>
-<script src="/js/vendor/livestamp.min.js"></script>
-<script src="/js/vendor/jquery.autosize.js"></script>
-<script src='/js/dnianas.js'></script>
-<script src="/js/init2.js"></script>
-<script type="text/javascript">Dnianas.Post.getNewPosts();</script>
-</body>
-</html>
