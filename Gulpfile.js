8e6d5186b3c0b8ac456c6d1a289747484543ecd1
AkarM13 <akarmathers13@gmail.com>
2015-05-07 11:24:44 +0300

    Updated Gulpfile to watch for styles and javascripts

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,16 +3,39 @@ var concat  = require('gulp-concat');
 var watch   = require('gulp-watch');
 var rootDir = 'app/assets/';
 var jsDir   = rootDir + 'javascripts/';
+var styleDir = 'public/style/';
 
 gulp.task('scripts', function () {
-    return gulp.src(jsDir + '**.js')
+    return gulp.src([
+            jsDir + '/vendor/moment.min.js',
+            jsDir + '/vendor/**.js',
+            jsDir + 'app.js',
+            jsDir + 'comment.js',
+            jsDir + 'notification.js',
+            jsDir + 'post.js',
+            jsDir + 'user.js',
+            jsDir + 'init.js'
+        ])
         .pipe(concat('application.js'))
-        .pipe(gulp.dest('./public/javascript'));
+        .pipe(gulp.dest('./public/js'));
+});
+
+gulp.task('styles', function () {
+    return gulp.src([
+            styleDir + 'index.css',
+            styleDir + 'style.css',
+            styleDir + 'jqstdniapara.css',
+            styleDir + 'header.css',
+            styleDir + 'media.css',
+        ])
+        .pipe(concat('application.css'))
+        .pipe(gulp.dest('./public/stylesheet'));
 });
 
 gulp.task('watch', function () {
    gulp.watch(jsDir + '*.js', ['scripts']);
+   gulp.watch(styleDir + '*.css', ['styles']);
 });
 
 // Default task
-gulp.task('default', ['scripts', 'watch']);
+gulp.task('default', ['styles', 'scripts', 'watch']);
