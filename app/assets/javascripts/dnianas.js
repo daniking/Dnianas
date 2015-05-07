370e42cbafebecf5164af02ee9ba0786f89a562f
AkarM13 <akarmathers13@gmail.com>
2015-05-07 17:48:21 +0300

    Move dnianas.js file from public directory to assets directory.

---
+++ app/assets/javascripts/dnianas.js
@@ -0,0 +1,168 @@
+$(function(){
+$('.normal').autosize();
+$('.comment-input').autosize();
+});
+$(document).ready(function(){
+	$("._192prosss").hide();
+});
+$('#clicksows').click(function(){ 
+	 $("._192prosss").fadeIn(600, function() {
+	$("._192prosss").show();
+	$(".hover_profile").hide();
+});
+});
+$('#clicksows').click(function(){ 
+$("body").css("position","absolute");
+
+});
+
+
+$(document).mouseup(function()
+{
+$("._192prosss").hide();
+$("._192prosss").fadeOut(600, function() {
+$("body").css("position","absolute");
+
+});
+});
+$(".profile_show_imagesclick").mouseup(function()
+{
+return false
+});
+
+ $(document).ready(function(){
+	$("#titlelicomresh").hide();
+});
+$('.sc-bot-like').click(function(){ 
+$("#titlelicomresh").fadeIn(600);
+$("._likes").css("opacity","1");
+});
+
+$(document).ready(function(){
+	$(".hover_profile").hide();
+});
+
+$(document).mouseup(function()
+{
+$(".hover_profile").hide();
+});
+
+$(document).ready(function(){
+	$(".logout_123").hide();
+});
+$("#logout").click(function(){
+	$(".logout_123").show();
+	$("#box_logout").fadeIn(600);
+	$("body").css("position","absolute");
+
+});
+
+$(".ui_buttonbtn_1").click(function(){
+	$(".logout_123").hide();
+	$("#box_logout").fadeIn(600);
+	$("body").css("position","relative");
+	});
+	
+	
+	
+ $(document).ready(function(){
+	$("#_whnb12").hide();
+});
+
+$("#opennotifii").click(function(){
+		 $('#_whnb12').show();
+});
+$("#_whnb12").mouseup(function()
+
+{
+return false
+});
+$(document).mouseup(function()
+{
+$("#_whnb12").hide();
+});
+
+$(document).ready(function(){
+	$(".message_shows").hide();
+});
+$("#openmesss").click(function(){
+	$(".message_shows").fadeIn(700);
+	$("body").css("position","absolute");
+
+});
+
+$("#closemesssage").click(function(){
+	$(".message_shows").fadeOut(700);
+	$("body").css("position","relative");
+});
+
+
+
+
+
+
+
+$(document).ready(function(){ 
+	$("#message-box-hide").hide();
+});
+
+$('#openmsg').click(function(){ 
+	$("#message-box-hide").show();
+	$("body").css("position","absolute");
+    $("body").css("left","0");
+    $("body").css("right","0");
+    $("body").css("top","0");
+
+});
+$('#clsoems').click(function(){ 
+	$("#message-box-hide").hide();
+	$("body").css("position","relative");
+	
+});
+
+$(document).ready(function(){ 
+	$(".spotlight").hide();
+});
+
+$('#openimgbig').click(function(){ 
+	$(".spotlight").show();
+	$("body").css("position","absolute");
+	$("body").css("overflow-x","hidden");
+	$("body").css("overflow","hidden");
+
+});
+
+$('#closeimgopen').click(function(){ 
+	$(".spotlight").hide();
+	$("body").css("position","relative");
+	$("body").css("overflow","auto");
+	
+});
+
+/*home style and jquery*/
+$(document).ready(function(){ 
+	$("#boxgalaprovshow").hide();
+});
+$('#opentag').click(function(){ 
+$("#boxgalaprovshow").show();
+});
+$('.closetag').click(function(){ 
+$("#boxgalaprovshow").hide();
+});
+
+$(document).ready(function(){ 
+	$(".moresectionuser").hide();
+});
+$('#opentargmore').click(function(){ 
+$(".moresectionuser").show();
+
+});
+$(".moresectionuser").mouseup(function()
+
+{
+return false
+});
+$(document).mouseup(function()
+{
+$(".moresectionuser").hide();
+});
