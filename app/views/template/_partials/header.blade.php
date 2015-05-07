f2e898ddb7f54c62805a6246e9760a1efa540dce
AkarM13 <akarmathers13@gmail.com>
2015-05-07 16:59:33 +0300

    Add query string to cache-bust the assets.

--- app/views/template/_partials/header.blade.php
+++ app/views/template/_partials/header.blade.php
@@ -3,7 +3,7 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
 <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-<link href="{{ asset('stylesheet/application.css') }}" rel="stylesheet">
+<link href="{{ asset('stylesheet/application.css?v=' . filemtime( 'stylesheet/application.css' )) }}" rel="stylesheet">
 <link rel="shortcut icon" href="http://faviconist.com/icons/a80270e82deef64f904f2b589880a4f3/favicon.ico" />
 <title>Dnianas</title>
 <link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
