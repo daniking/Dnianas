2924d2854d970da239542251af310c06914e3b4c
AkarM13 <akarmathers13@gmail.com>
2015-05-14 16:12:40 +0300

    Fix spelling

--- app/assets/javascripts/notification.js
+++ app/assets/javascripts/notification.js
@@ -8,11 +8,7 @@ Dnianas.Notification = {
     },
 
     countNotifications: function () {
-<<<<<<< HEAD
-        var $notifications = $('.boxnotificationsusers').children().find('.boxsendnotifi');
-=======
-        var zzz$notifications = $('.boxnotificationsusers').children().find('.boxsendnotifi');
->>>>>>> c96a3857a13903459af557f6f0a7acc4f5b11a17
+        var $notifications = $('.boxnotificationsusers').children().find('.boxsendnotifi')
         var ids = [];
 
         // Add unread notifications to the ids array.
