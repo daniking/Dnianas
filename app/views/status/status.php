59f5365364443172dd0bd14ed77a2ad078971e9f
Danyal <danyalart@gmail.com>
2015-05-23 13:06:59 +0300

    status

---
+++ app/views/status/status.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<link href="../../../public/style/other.css" rel="stylesheet">
+<title>User-post</title>
+</head>
+
+<body>
+<div class="maincountlong">
+<div class="longstatus">
+
+</div>
+<div class="taglinkdnia"><div  id="fo404_234">
+<span>Dnianas Corporation ©2015</span>
+<span>|</span>
+<a href="">About</a>
+<span>-</span>
+<a href="">Help</a>
+<span>-</span>
+<a href="">Terms</a>
+<span>-</span>
+<a href="">Cookies</a>
+<span>-</span>
+<a href="">Privacy</a>
+<span>-</span>
+<a href="">advertising</a>
+</div></div>
+</div>
+
+
+</body>
+</html>
